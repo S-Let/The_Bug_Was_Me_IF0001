172b42b65fc11744b5e4cf94d245fb29b5635240
Luis Pa <Luispablo.vargas@ucr.ac.cr>
2026-06-14 17:53:30 -0600

    metodos y atributos a gamecontroller y consoleview

--- src/cr/ac/ucr/paraiso/The_Bug_Was_Me/controller/GameController.java
+++ src/cr/ac/ucr/paraiso/The_Bug_Was_Me/controller/GameController.java
@@ -1,4 +1,44 @@
 package cr.ac.ucr.paraiso.The_Bug_Was_Me.controller;
 
+import cr.ac.ucr.paraiso.The_Bug_Was_Me.model.Enemy;
+import cr.ac.ucr.paraiso.The_Bug_Was_Me.model.Hero;
+import cr.ac.ucr.paraiso.The_Bug_Was_Me.model.Map;
+import cr.ac.ucr.paraiso.The_Bug_Was_Me.view.ConsoleView;
+
 public class GameController {
+    private Hero hero;
+    private Map map;
+    private Enemy[] enemies;
+    private ConsoleView console;
+
+    public GameController(Hero hero, Map map, Enemy[] enemies, ConsoleView console) {
+        this.hero = hero;
+        this.map = map;
+        this.enemies = enemies;
+        this.console = console;
+    }
+    public void startGame(){
+        // inicia juego
+    }
+
+    public void processTurn(){
+        // controlar turno del jugador
+    }
+    public void checkCombat(){
+        // verifica si existe un combate
+    }
+    public void checkVictory(){
+        // verifica victoria
+    }
+    public void checDefeat(){
+        //verifica derrota
+    }
+    public void saveGame(){
+        //Guarda el estado del juego
+    }
+    public void loadGame(){
+        // Carga estado del juego
+    }
+
+
 }
--- src/cr/ac/ucr/paraiso/The_Bug_Was_Me/view/ConsoleView.java
+++ src/cr/ac/ucr/paraiso/The_Bug_Was_Me/view/ConsoleView.java
@@ -1,4 +1,25 @@
 package cr.ac.ucr.paraiso.The_Bug_Was_Me.view;
 
+import cr.ac.ucr.paraiso.The_Bug_Was_Me.model.Hero;
+import cr.ac.ucr.paraiso.The_Bug_Was_Me.model.Map;
+
 public class ConsoleView {
+    public void displayMap(Map map){
+        // Mostrar el mapa
+    }
+
+    public void displayHeroStatus(Hero hero){
+        //mostrar vida, oro, etc.
+    }
+
+    public void displayMessage(String message){
+        System.out.println(message);
+    }
+
+    public void displayMenu(){
+        // mostrar opciones: mover, atacar, usar item...
+    }
+    public void clearScreen(){
+        // limpiar pantalla
+    }
 }
